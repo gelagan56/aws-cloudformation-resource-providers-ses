f3a41d5ee7aa0de071faf934125941196f1b8fb2
Chi Zhang <zhngchi@amazon.com>
2020-08-01 01:56:12 +0000

    Init resource handlers

---
+++ aws-testservice-testresource/src/main/java/software/amazon/testservice/testresource/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.testservice.testresource;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-testservice-testresource.json");
+    }
+}
